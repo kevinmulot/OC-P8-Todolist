1e2873698262959eec5d13a607dd975b2a0ded2a
Kevin Mulot <kevin.mulot@gmail.com>
2021-07-13 14:09:52 +0200

    Unit tests

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Tests\Controller;
 
 use App\Entity\User;
@@ -13,10 +12,19 @@ class DefaultControllerTest extends WebTestCase
     public function setUp(): void
     {
         $this->client = static::createClient();
+        $this->login = new UserControllerTest();
+    }
+
+    public function loginUser(): void
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $this->client->submit($form, ['username' => 'admin', 'password' => 'adminpass']);
     }
 
     public function testIndex()
     {
+        $this->loginUser();
 
         $this->client->request('GET', '/');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
